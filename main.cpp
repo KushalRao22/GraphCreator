1d8aafcdbb8524964988b6959d9b7f16804e8c82
KushalRao22 <90669418+KushalRao22@users.noreply.github.com>
2022-06-11 16:37:24 -0700

    Added Edges and Verts

--- main.cpp
+++ main.cpp
@@ -1,75 +1,96 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
 
 using namespace std;
 
-struct vert{
-  char name[100];
+
+struct vert {
+	char name[50];
+	vector <struct edge*> edges;
 };
 
-struct edge{
-  vert* vert1 = NULL;
-  vert* vert2 = NULL;
-  int weight = 0;
+struct edge {
+	vert* start;
+	vert* end;
+	int weight;
 };
-
-
-void aVertex(vert** verts, int &size);
-void aEdge(vert** verts, edge** edges);
-void rmVertex(vert** verts);
-void rmEdge(vert** verts, edge** edges);
-
 
 int main(){
-  vert** verts = new vert*[1];
-  edge** edges = new edge*[1];
-  char input[7];
-  bool quit = false;
-  int size = 0;
-  while(!quit){
-    cout << "Type a valid command(AE(ADD EDGE), AV(ADD VERTEX), PRINT, RE(REMOVE EDGE), RV(REMOVE VERTEX), SP(SHORTEST PATH) QUIT)" << endl;//Prompt user for input
-    cin >> input;
-    if(strcmp(input, "AE") == 0){
-      
-    }
-    if(strcmp(input,"AV") == 0){//If user wants to add students
-      vert** temp = new vert*[(sizeof(verts)/sizeof(verts[0]))+1];
-      cout << (sizeof(verts)/sizeof(verts[0])) << endl;
-      delete verts;
-      verts = temp;
-      cout << (sizeof(verts)/sizeof(verts[0])) << endl;
-      aVertex(verts, size);
-    }
-    else if(strcmp(input, "RE")==0){
-      
-    }
-    else if(strcmp(input,"RV") == 0){//If user wants to delete a specific student
-      
-    }
-    else if(strcmp(input,"PRINT") == 0){//If user wants to print a specific student
-      cout << (sizeof(verts)/sizeof(verts[0])) << endl;
-      for(int i = 0; i < (sizeof(verts)/sizeof(verts[0])); i ++){
-	cout << verts[i]->name << endl;
-      }
-    }
-    else if(strcmp(input,"SP") == 0){//If user wants to print a specific student
-      
-    }
-    else if(strcmp(input,"QUIT") == 0){//If user wants to quit
-      quit = true;
-    }
-  }
-  
-  return 0;
-}
+	vector <vert*> verts;
+	char input[50];
+	bool quit = false;
+	int size = 0;
+	while(!quit){
+		cout << "Type a valid command(AE(ADD EDGE), AV(ADD VERTEX), PRINT, RE(REMOVE EDGE), RV(REMOVE VERTEX), SP(SHORTEST PATH) QUIT)" << endl;//Prompt user for input
+		cin >> input;
+		if(strcmp(input, "AE") == 0){
+			vert* start = NULL;
+			vert* end = NULL;
+			bool temp = true;
+			bool temp2 = true;
+			while (temp) {
+				cout << "Enter the starting node: "; 
+				cin >> input;
+				for (int i=0; i<verts.size(); i++) {
+					if (strcmp(verts[i]->name,input) == 0) {
+						start = verts[i];
+					}
+				}
+				if (start == NULL) {
+					cout << "No node has that name." << endl;
+				}
+				else {
+					temp = false;
+				}
+			}
+			while (temp2) {
+				cout << "Enter the end node: "; 
+				cin >> input;
+				for (int i=0; i<verts.size(); i++) {
+					if (strcmp(verts[i]->name,input) == 0) {
+						end = verts[i];
+					}
+				}
+				if (end == NULL) {
+					cout << "No node has that label." << endl;
+				}
+				else {
+					temp2 = false;
+				}
+			}
+			int num;
+			cout << "Enter the weight of the edge: "; 
+			cin >> num;
+			edge* n = new edge();
+			n->start = start;
+			n->end = end;
+			n->weight = num;
+			start->edges.push_back(n);
+		}
+		if(strcmp(input,"AV") == 0){//If user wants to add students
+			cout << "Enter the label for the new vertex: "; 
+			cin >> input;
+			vert* n = new vert();
+			strcpy(n->name, input);
+			verts.push_back(n);
 
-void aVertex(vert** verts, int &size){
-  char temp[100];
-  cout << "What is the name of the vertex" << endl;
-  cin >> temp;
-  cin.clear();
-  vert* n = new vert;
-  strcpy(n->name, temp);
-  verts[size] = n;
-  size++;
+		}
+		else if(strcmp(input, "RE")==0){
+
+		}
+		else if(strcmp(input,"RV") == 0){//If user wants to delete a specific student
+
+		}
+		else if(strcmp(input,"PRINT") == 0){//If user wants to print a specific student
+		}
+		else if(strcmp(input,"SP") == 0){//If user wants to print a specific student
+
+		}
+		else if(strcmp(input,"QUIT") == 0){//If user wants to quit
+			quit = true;
+		}
+	}
+
+	return 0;
 }
